bc23141d9b09d52d14fbd68ea7e95425452f60eb
Jack Adamczyk <prepinavest@gmail.com>
2024-08-09 10:45:56 -0400

    add weather cards

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,17 @@
+import WeatherCard from "../WeatherCard/WeatherCard";
+import "./Main.css";
+
+function Main() {
+  return (
+    <main>
+      <WeatherCard />
+      <section className="cards">
+        <p className="cards__text">
+          Today is 75 &deg; F / You may want to wear:
+        </p>
+        {/* Add the card component here */}
+      </section>
+    </main>
+  );
+}
+export default Main;
