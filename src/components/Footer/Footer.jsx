8062fcc37945d6a721a7878f2df4f55ecde5a35e
Jack Adamczyk <prepinavest@gmail.com>
2024-08-09 09:06:05 -0400

    Update Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__dev">Developed by Name Surname</p>
+      <p className="footer__date">2024</p>
+    </footer>
+  );
+}
+
+export default Footer;
