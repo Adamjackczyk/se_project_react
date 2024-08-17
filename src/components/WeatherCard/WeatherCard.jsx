3ef40649ab1c61574bf2c2370c86b762f5be729b
Jack Adamczyk <prepinavest@gmail.com>
2024-08-17 05:15:36 -0400

    Add new cards based on weather

--- src/components/WeatherCard/WeatherCard.jsx
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -1,11 +1,36 @@
-import sunny from "../../assets/sunny.svg";
 import "./WeatherCard.css";
+import { weatherOptions, defaultWeatherOptions } from "../../utils/constants";
 
 function WeatherCard({ weatherData }) {
+  const filteredOptions = weatherOptions.filter((option) => {
+    return (
+      option.day === weatherData.isDay &&
+      option.condition === weatherData.condition
+    );
+  });
+
+  let weatherOptionUrl;
+  let weatherOptionCondition;
+
+  if (filteredOptions.length === 0) {
+    weatherOptionUrl =
+      defaultWeatherOptions[weatherData.isDay ? "day" : "night"].url;
+    weatherOptionCondition = defaultWeatherOptions.condition;
+  } else {
+    weatherOptionUrl = filteredOptions[0]?.url;
+    weatherOptionCondition = filteredOptions[0]?.condition;
+  }
+
   return (
     <section className="weather-card">
       <p className="weather-card__temp">{weatherData.temp.F} &deg; F</p>
-      <img src={sunny} alt={weatherData.type} className="weather-card__image" />
+      <img
+        src={weatherOptionUrl}
+        alt={`card showing ${weatherOptionCondition} weather at ${
+          weatherData.isDay ? "day" : "night"
+        }`}
+        className="weather-card__image"
+      />
     </section>
   );
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,76 @@
+export const weatherOptions = [
+  {
+    day: true,
+    condition: "clear",
+    url: new URL("../assets/day/clear.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "cloudy",
+    url: new URL("../assets/day/cloudy.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "fog",
+    url: new URL("../assets/day/fog.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "rain",
+    url: new URL("../assets/day/rain.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "snow",
+    url: new URL("../assets/day/snow.svg", import.meta.url).href,
+  },
+  {
+    day: true,
+    condition: "strom",
+    url: new URL("../assets/day/storm.svg", import.meta.url).href,
+  },
+  //night
+  {
+    day: false,
+    condition: "clear",
+    url: new URL("../assets/night/clear.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "cloudy",
+    url: new URL("../assets/night/cloudy.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "fog",
+    url: new URL("../assets/night/fog.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "rain",
+    url: new URL("../assets/night/rain.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "snow",
+    url: new URL("../assets/night/snow.svg", import.meta.url).href,
+  },
+  {
+    day: false,
+    condition: "strom",
+    url: new URL("../assets/night/storm.svg", import.meta.url).href,
+  },
+];
+
+export const defaultWeatherOptions = {
+  day: {
+    url: new URL("../assets/day/default.svg", import.meta.url).href,
+  },
+  night: {
+    url: new URL("../assets/night/default.svg", import.meta.url).href,
+  },
+};
+
 export const defaultClothingItems = [
   {
     _id: 0,
@@ -40,6 +113,10 @@ export const defaultClothingItems = [
 export const coordinates = {
   latitude: 32.466679,
   longitude: -84.990402,
+  // Phenix City
+  // latitude: 51.509865,
+  // longitude: -0.118092,
+  //london ^^
 };
 
 export const APIkey = "c17a71d4622d559b5c6fdc0167bff5cf";
