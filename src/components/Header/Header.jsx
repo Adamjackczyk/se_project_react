d2ec07e7add1576326ff9d1727b65d27c14c9def
Jack Adamczyk <prepinavest@gmail.com>
2024-08-31 15:58:06 -0400

    add links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.svg";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import { Link } from "react-router-dom";
 
 function Header({ handleAddClick, weatherData }) {
   let name = "Terrence Tegegne";
@@ -13,7 +14,9 @@ function Header({ handleAddClick, weatherData }) {
   return (
     <header className="header">
       <div className="header__left">
-        <img className="header__logo" src={logo} alt="logo" />
+        <Link to="/">
+          <img className="header__logo" src={logo} alt="logo" />
+        </Link>
         <p className="header__date-and-location">
           {currentDate}, {weatherData.city}
         </p>
@@ -27,10 +30,12 @@ function Header({ handleAddClick, weatherData }) {
         >
           + Add clothes
         </button>
-        <div className="header__user-container">
-          <p className="header__username">{name}</p>
-          <img src={avatar} alt={name} className="header__avatar" />
-        </div>
+        <Link to="/profile" className="header__profile-link">
+          <div className="header__user-container">
+            <p className="header__username">{name}</p>
+            <img src={avatar} alt={name} className="header__avatar" />
+          </div>
+        </Link>
       </div>
     </header>
   );
