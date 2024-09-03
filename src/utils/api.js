799dab60ce6cf4f7ca059a2071f73f138df5b37a
Jack Adamczyk <prepinavest@gmail.com>
2024-09-03 01:04:32 -0400

    changed api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,51 +1,32 @@
 const baseUrl = "http://localhost:3001";
 
+function checkResponse(res) {
+  if (res.ok) {
+    return res.json();
+  }
+  return Promise.reject(`Error ${res.status}: ${res.statusText}`);
+}
+
+function request(url, options) {
+  return fetch(url, options).then(checkResponse);
+}
+
 export function getItems() {
-  return fetch(`${baseUrl}/items`)
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(`Error fetching items: ${response.statusText}`);
-    })
-    .catch((error) => {
-      console.error(error);
-      throw error;
-    });
+  return request(`${baseUrl}/items`);
 }
 
 export function addItem(newItem) {
-  return fetch(`${baseUrl}/items`, {
+  return request(`${baseUrl}/items`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(newItem),
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(`Error adding item: ${response.statusText}`);
-    })
-    .catch((error) => {
-      console.error(error);
-      throw error;
-    });
+  });
 }
 
 export function deleteItem(id) {
-  return fetch(`${baseUrl}/items/${id}`, {
+  return request(`${baseUrl}/items/${id}`, {
     method: "DELETE",
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error(`Error deleting item: ${response.statusText}`);
-    })
-    .catch((error) => {
-      console.error(error);
-      throw error;
-    });
+  });
 }
