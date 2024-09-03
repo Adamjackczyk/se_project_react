fbcf924ef03d9eff66949c889fce84f9d09b3f23
Jack Adamczyk <prepinavest@gmail.com>
2024-09-03 00:58:02 -0400

    fixed size issue

--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -1,12 +1,19 @@
+import "./ItemCard.css";
+
 function ItemCard({ item, onCardClick }) {
   const handleCardClick = () => {
     onCardClick(item);
   };
 
   return (
-    <li className="card" onClick={handleCardClick}>
-      <img className="card__image" src={item.imageUrl} alt={item.name} />
+    <li className="card">
       <h2 className="card__name">{item.name}</h2>
+      <img
+        onClick={handleCardClick}
+        className="card__image"
+        src={item.imageUrl}
+        alt={item.name}
+      />
     </li>
   );
 }
